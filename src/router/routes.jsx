9f99973f3c51e9484708e3c3096cf0e6425f1317
Mahel Chandupa <mahelchandupa1013@gmail.com>
2024-06-23 17:07:10 +0530

    create pages and routes

---
+++ src/router/routes.jsx
@@ -0,0 +1,38 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home";
+import SignUp from "../pages/SignUp";
+import SignIn from "../pages/SignIn";
+import About from "../pages/About";
+import Profile from "../pages/Profile";
+import Root from "./root";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root />,
+        children: [
+           {
+             path: '/',
+             element: <Home />
+           },
+           {
+            path: '/sign-up',
+            element: <SignUp />
+           },
+           {
+            path: '/sign-in',
+            element: <SignIn />
+           },
+           {
+            path: '/about',
+            element: <About />
+           },
+           {
+            path: '/profile',
+            element: <Profile />
+           }
+        ]
+    }
+]);    
+
+export default router;
