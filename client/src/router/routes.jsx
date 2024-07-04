696505876a29d677efdc9173df2066b265dc7635
Mahel Chandupa <mahelchandupa1013@gmail.com>
2024-07-04 19:52:41 +0530

    update the header and make profile page private

--- client/src/router/routes.jsx
+++ client/src/router/routes.jsx
@@ -5,34 +5,39 @@ import SignIn from "../pages/SignIn";
 import About from "../pages/About";
 import Profile from "../pages/Profile";
 import Root from "./root";
+import PrivateRoute from "../components/PrivateRoute";
 
 const router = createBrowserRouter([
-    {
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
         path: '/',
-        element: <Root />,
-        children: [
-           {
-             path: '/',
-             element: <Home />
-           },
-           {
-            path: '/sign-up',
-            element: <SignUp />
-           },
-           {
-            path: '/sign-in',
-            element: <SignIn />
-           },
-           {
-            path: '/about',
-            element: <About />
-           },
-           {
-            path: '/profile',
-            element: <Profile />
-           }
-        ]
-    }
-]);    
+        element: <Home />
+      },
+      {
+        path: '/sign-up',
+        element: <SignUp />
+      },
+      {
+        path: '/sign-in',
+        element: <SignIn />
+      },
+      {
+        path: '/about',
+        element: <About />
+      },
+      {
+        path: '/profile',
+        element: (
+          <PrivateRoute>
+            <Profile />
+          </PrivateRoute>
+        )
+      }
+    ]
+  }
+]);
 
 export default router;
