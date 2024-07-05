f7ccd00fd800ddd73953689d410cb37d905286d1
Mahel Chandupa <mahelchandupa1013@gmail.com>
2024-07-05 15:11:04 +0530

    add delete user functionallity

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -9,9 +9,9 @@ export const test = (req, res) => {
 }
 
 export const updateUser = async (req, res, next) => {
-    
+
     if (req.user.id != req.params.id) return next(errorHandler(401, 'You can only update your own profile!'))
-    
+
     try {
         if (req.body.password) {
             req.body.password = bcryptjs.hashSync(req.body.password, 10)
@@ -24,11 +24,23 @@ export const updateUser = async (req, res, next) => {
                 password: req.body.password,
                 avatar: req.body.avatar
             }
-        }, { new: true }) 
+        }, { new: true })
 
         const { password, ...others } = updatedUser._doc
         res.status(200).json(others)
 
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const deleteUser = async (req, res, next) => {
+    if (req.user.id !== req.params.id) return next(errorHandler(401, 'You can only delete your own profile!'))
+
+    try {
+        await User.findByIdAndDelete(req.params.id)
+        res.clearCookie('access_token')
+        res.status(200).json('User has been deleted!')
     } catch (error) {
         next(error)
     }
