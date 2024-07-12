8c66a71a6b36ec12c9ca91859982a3ebde1697d7
Mahel Chandupa <mahelchandupa1013@gmail.com>
2024-07-12 18:45:23 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div className=' py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className=' text-3xl font-bold mb-4 text-slate-800'>About Land Estate</h1>
+      <p className=' text-lg mb-4 text-slate-700 '>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi distinctio iusto, harum adipisci quod ipsum totam eveniet et exercitationem cumque dolorum omnis laborum debitis? Dolores earum magnam consequuntur in culpa.</p>
+      <p className=' text-lg mb-4 text-slate-700 '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci quos saepe id debitis rem iusto est reiciendis enim porro blanditiis impedit sed deleniti temporibus quo animi in, consequatur quod eveniet</p>
+      <p className=' text-lg mb-4 text-slate-700 '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci quos saepe id debitis rem iusto est reiciendis enim porro blanditiis impedit sed deleniti temporibus quo animi in, consequatur quod eveniet</p>
+    </div>
   )
 }
 
